7f83dfe8edd0c947a3dbe223fe9fcda387ad8724
mattiacreates <mattia.raemy@gmail.com>
2025-07-04 14:13:22 +0200

    Fix stub tool to support invoke

--- langchain_core/tools.py
+++ langchain_core/tools.py
@@ -1,2 +1,18 @@
+class Tool:
+    """Minimal tool wrapper providing an ``invoke`` method."""
+
+    def __init__(self, func):
+        self.func = func
+        self.__doc__ = getattr(func, "__doc__")
+        self.__name__ = getattr(func, "__name__", "Tool")
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    def invoke(self, *args, **kwargs):  # pragma: no cover - simple passthrough
+        return self.func(*args, **kwargs)
+
+
 def tool(func):
-    return func
+    """Return a :class:`Tool` instance for the decorated function."""
+    return Tool(func)
