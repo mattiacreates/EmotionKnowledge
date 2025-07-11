3ce8ef7c1c7f7d36eeb153a5bd0d8d301a5b15ad
mattiacreates <mattia.raemy@gmail.com>
2025-07-11 09:52:24 +0200

    Fix text emotion model default

--- emotion_knowledge/emotion_models.py
+++ emotion_knowledge/emotion_models.py
@@ -24,7 +24,7 @@ class EmotionModel:
 class TextEmotionModel:
     """Wrapper around a text-classification pipeline for emotion detection."""
 
-    def __init__(self, model_name: str = "oliverguhr/german-emotion-bert") -> None:
+    def __init__(self, model_name: str = "oliverguhr/german-sentiment-bert") -> None:
         self.classifier = pipeline("text-classification", model=model_name)
 
     def predict(self, text: str) -> tuple[str, float]:
